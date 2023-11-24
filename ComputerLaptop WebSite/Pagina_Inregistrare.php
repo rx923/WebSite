cf7e5c43f7465ed79f92853406a0d65904fb857c
rx923 <rx923@proton.me>
2023-11-24 08:43:48 +0200

    adjusted the code

--- ComputerLaptop WebSite/Pagina_Inregistrare.php
+++ ComputerLaptop WebSite/Pagina_Inregistrare.php
@@ -1,6 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
+    $_SERVER = [""];
     $psw = $_POST['psw'];
     $servername = "localhost";
     $pswRepeat = $_POST['psw-repeat'];
