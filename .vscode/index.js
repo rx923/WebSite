c1647aa4bd152f4a576b7af3da7d44deacca248f
rx923 <rx923@proton.me>
2024-02-06 19:58:42 +0200

    Adjusted styles

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
+// Importing the cors module
+const cors = require("cors"); 
 const app = express();
-const app = require ("cors");
 
-//middleware
-app.use(cors());
+// Middleware
+// Applying CORS middleware
+app.use(cors()); 
 app.use(express.json());
 
-
 app.listen(5432, () => {
-    console.log("server started on port 5432");
+    console.log("Server started on port 5432");
 });
-
