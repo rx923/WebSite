4d22a11a3f871bb4ca40b21b360c84a22716d285
rx923 <rx923@proton.me>
2024-02-09 22:06:39 +0200

    Adjusted the index page and added a side menu arrow for opening the main menu. Modified the style of the menu that opens on the screen.

--- .vscode/index.js
+++ .vscode/index.js
@@ -9,6 +9,17 @@ app.use(cors());
 app.use(express.static('public')); 
 app.use(express.json());
 
+
+
+// Defining a route to serve the Logare.html file:
+app.get('\\Logare', (req, res) =>{
+    res.sendFile('U:\\Plan Afacere\\WebSite\\ComputerLaptop WebSite\\Inregistrare&Logare_user\\Inregistrare.html', {root: --__dirname});
+});
+
+
+
+
+//Enabling application to listen on port 8080
 app.listen(8080, () => {
     console.log("Server started on port 8080");
 });
