da9e344bed7fcf917d558a808a1b332d4fb062b7
rx923 <rx923@proton.me>
2024-02-11 01:06:15 +0200

    Database Configured. Connection Successful. Database Design next step

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const {Client} = require('pg');
+//Loading the environment variables from .env file
+require('dotenv').config();
+
+
+const client = new Client({
+    user: process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
+});
+
+module.exports = client;
