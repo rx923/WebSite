7f47605a3e02ff0ba35b4162094755c7dc054f5f
rx923 <rx923@proton.me>
2024-02-14 01:49:57 +0200

    adjusted multiple settings

--- .vscode/index.js
+++ .vscode/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const { Sequelize, DataTypes } = require('sequelize');
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
@@ -47,8 +49,25 @@ app.post('/register', async (req, res) => {
   }
 });
 
-// Start the server
+// Start the express server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Express server started on port ${PORT}`);
 });
+
+// Start the live-http server
+var liveServer = require("live-server");
+
+var params = {
+  port: 8080, // Set the server port. Defaults to 8080.
+  host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+  root: "/public", // Set root directory that's being served. Defaults to cwd.
+  open: false, // When false, it won't load your browser by default.
+  ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+  file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+  wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+  mount: [['/components', './node_modules']], // Mount a directory to a route.
+  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+  middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+liveServer.start(params);
