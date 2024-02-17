3495c55ab3ae46e4ac104e6baa61a0fdd3e4b3b1
rx923 <rx923@proton.me>
2024-02-17 05:21:57 +0200

    Database connection functions mainly ok. Checkpoint 1. Creation of tables inside the Database works. Checkpoint 2. User submit form registration mapping to the database tables needs to be implemented.

--- server/server.js
+++ server/server.js
@@ -4,18 +4,69 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+//Middleware
+
+app.use(cors());
+// The below code serves the static files.
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+
+//Creating a database connection
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: process.env.DB_HOST || '192.168.100.53',
+    prot: process.env.DB_PORT || '5432',
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'MainAdministrator',
+    database: process.env.DB_NAME || 'AccountCreation',
+});
+
+// Defining the User model
+
+const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+});
+
+
+
+//Synchornizing the model with the database (create the table if it doesn't exist)
+(async () => {
+    await sequelize.sync({alert: true});
+    console.log('Database synchornized');
+})();
+
+//Route to serve the Logare.html file
+app.get('/WebSite/ComputerLaptop_WebSite/Inregistrare&Logare_user/Logare.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Inregistrare&Logare_user', 'Inregistrare.html'));
+});
+
+
+//Route to handle the user registration
+app.post('/WebSite/ComputerLaptop_WebSite/Inregistrare&Logare_user/Inregistrare.html', async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+      const newUser = await User.create({ username, email, password });
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
+  
 // Defining the routes
 
 
-const PORT = process.env.PORT || 8081;
-const HOST = '192.168.100.53';
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
-
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, HOST, () =>{
     console.log(`Server is running on http://localhost:${PORT}`);
 });
